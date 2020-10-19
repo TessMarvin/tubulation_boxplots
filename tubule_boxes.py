a3f7f14d29964cfec9ba9d2c65d2fc9af1abaded
TessMarvin <tmarvin@nd.edu>
2020-10-19 15:59:44 -0400

    Added ANOVA and better x-axis jitter

--- tubule_boxes.py
+++ tubule_boxes.py
@@ -1,5 +1,5 @@
 #Author: Tess Marvin (tmarvin@nd.edu)
-#Usage:
+#Usage: python tubule_boxes.py
 #Purpose: Crate box and whisker plot of tubulation data for Vaughan Lab
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -7,6 +7,8 @@ from matplotlib import cm
 from argparse import ArgumentParser
 import numpy as np
 from scipy import stats
+#from statsmodels.stats.multicomp import pairwise_tukeyhsd
+from collections import Counter
 
 def boxplot_hero(tubdata):
     '''
@@ -19,14 +21,50 @@ def boxplot_hero(tubdata):
             medianprops=dict(color=c))
     clevels = np.linspace(0., 1., tubdata.shape[0])
     columns=list(tubdata)
+    '''
+    This was for a random jitter along the x axis for each box -- didn't look as good as I had hoped
+
     for i in range(1,tubdata.shape[1]+1):
         y=tubdata[columns[i-1]]
-        x=np.random.normal(i, 0.04, size=len(y))
+        x=np.random.normal(i, 0.07, size=len(y))
         plt.scatter(x,y,alpha=0.5, color="black")
-    #Here we want to do an ANOVA
+    '''
+    '''
+    This is a non-random jitter on the x-axis -- hopefully it looks good
+    '''
+    for i in range(1,tubdata.shape[1]+1):
+        y=sorted(tubdata[columns[i-1]].tolist())
+        coun = Counter(y)
+        x=[]
+        k=0.04
+        pos = True
+        j=0
+        while j < len(y):
+            if coun[y[j]] == 1:
+                x.append(i)
+                j=j+1
+            else:
+                    for n in range(coun[y[j]]):
+                        if(pos):
+                            x.append(i+(n*k))
+                            pos= not pos
+                        else:
+                            x.append(i-(n*k))
+                            pos= not pos
+                    j = j+coun[y[j]]
+        plt.scatter(x,y,alpha=0.5, color="black")
+    #Edit Needed: make this a command line arg
+    bp.set(ylabel='Tubule Count')
+    #Edit Needed: make this a command line arg
+    bp.set(title='Effect of STARD9 Knockdown on Lysosomal Membrane Tubulation')
+    #Here we want to do an ANOVA because there are more than two groups
+    #A list to hold the list of data points for each cell type
+    li = []
+    #Fill up the empty list so it looks like this [[data for cell type],[data for next cell type], [data for last]]
     for c in columns:
-        print(tubdata[c])
-    f,p = stats.f_oneway(tubdata[c] for c in columns)
+        li.append(tubdata[c].tolist())
+    f,p = stats.f_oneway(*[li[l] for l in range(len(li))])
+    print(f,p)
     plt.show()
 if __name__ == '__main__':
     raw_data= './tubule_data.csv'
